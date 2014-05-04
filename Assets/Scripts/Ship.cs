2ae35ebdf9202589ea24789f9796211d3c4fa19c
Sagiv Ofek <sagiv@Sagivs-MacBook-Pro.local>
2014-05-03 20:20:50 -0400

    first commit

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour {
+
+	public float Speed = 200;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody2D.velocity = new Vector2(Input.GetAxis("Horizontal")*Speed*Time.deltaTime, Input.GetAxis("Vertical")*Speed*Time.deltaTime);
+	}
+}
