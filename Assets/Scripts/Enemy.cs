588b656029c476cea295bbe41dee97758598b026
Sagiv Ofek <sagiv@Sagivs-MacBook-Pro.local>
2014-05-03 21:20:05 -0400

    following camera

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
-	public float Speed = 2;
+	public float Speed = 5;
 	public GameObject Player;
 
 	// Use this for initialization
@@ -14,9 +14,5 @@ public class Enemy : MonoBehaviour {
 	void Update () {
 		transform.LookAt(Player.transform.position);
 		transform.position += transform.forward*Speed*Time.deltaTime;
-
-		//thisTransform.position.x = Mathf.Lerp( thisTransform.position.x, Player.position.x + xOffset, Time.deltaTime);
-		//thisTransform.position.y = Mathf.Lerp( thisTransform.position.y, Player.position.y + yOffset, Time.deltaTime);
-
 	}
 }
