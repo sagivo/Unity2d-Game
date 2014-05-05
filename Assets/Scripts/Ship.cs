3e865237d781d5228f275dede0421039ca5d56be
sagivo <sagiv4@gmail.com>
2014-05-05 16:11:17 -0400

    added canon

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -5,26 +5,31 @@ public class Ship : MonoBehaviour {
 
 	public float Speed = 2000;
 	public GameObject Bullet;
-	
+	public GameObject Canon;
+
+	bool canPutCanon = false;
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		rigidbody2D.velocity = new Vector2(Input.GetAxis("Horizontal")*Speed*Time.deltaTime, Input.GetAxis("Vertical")*Speed*Time.deltaTime);
+		if(Input.GetButtonDown("Fire1")) putCanon();
 		if(Input.GetButtonDown("Jump")) shot();				
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
-		Debug.Log("uuu");
 		if (other.gameObject.tag == "Boundary") Debug.Log("BOOM!");
 	}
 
 	void shot()
 	{
 		Instantiate(Bullet, transform.position, transform.rotation);
+	}
+
+	void putCanon(){
+		Instantiate(Canon, transform.position, transform.rotation);
 	}
 }
