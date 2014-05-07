3ef405317a42f841f3206e6e854df13de7646ddb
Alex Trost <admin@admin.com>
2014-05-07 22:08:32 +0300

    Moved the spawns to a sub layer Added emptyes to the game structure. * Somehow i lost the light. So i added another
    Signed-off-by: Alex Trost <admin@admin.com>

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -2,27 +2,40 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Game : MonoBehaviour {
-	public static Game Instance;
-	public int SpawnEnemiesEvery = 3;
-	public GameObject EnemyToSpwn;
-	public List<GameObject> Enemies;
+public class Game : MonoBehaviour
+{
+		public static Game Instance;
+		public int SpawnEnemiesEvery = 3;
+		public GameObject EnemyToSpwn;
+		public List<GameObject> Enemies;
 
-	void Awake(){
-		Instance = this;
-	}
+		private GameObject spawnLayer;
+		
 
-	void Start () {
-		Enemies = new List<GameObject>();
-		if (!EnemyToSpwn) EnemyToSpwn = GameObject.FindWithTag("Enemy");
-		spawnEnemy();
-	}
+		void Awake ()
+		{
+				Instance = this;
+		}
+
+		void Start ()
+		{
+				spawnLayer = GameObject.FindGameObjectWithTag ("SPAWNS");
+				Enemies = new List<GameObject> ();
+				spawnEnemy ();
+		}
 	
-	void Update () {
-	}
+		void Update ()
+		{
 
-	void spawnEnemy(){
-		Enemies.Add( (GameObject)Instantiate(EnemyToSpwn,new Vector3(Random.Range(-40f,40f),Random.Range(-40f,40f),-1f),Quaternion.identity) );
-		Invoke("spawnEnemy",Random.Range(0,SpawnEnemiesEvery));
-	}
+		}
+
+		void spawnEnemy ()
+		{
+				GameObject enemy = (GameObject)Instantiate (EnemyToSpwn, new Vector3 (Random.Range (-40f, 40f), Random.Range (-40f, 40f), -1f), Quaternion.identity);
+
+				Enemies.Add (enemy);
+				enemy.transform.parent = spawnLayer.transform;
+
+				Invoke ("spawnEnemy", Random.Range (0, SpawnEnemiesEvery));
+		}
 }
