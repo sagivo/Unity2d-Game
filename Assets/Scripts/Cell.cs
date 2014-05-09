0dce855f150be594edb96d3213725ac740c7d1a1
sagivo <sagiv4@gmail.com>
2014-05-09 15:37:02 -0400

    cell logic added

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -5,7 +5,19 @@ public class Cell : BaseObj {
 
 	public enum CellType {Empty, Player, Canon, Building};
 	public CellType Type;
+	SpriteRenderer sprite;
 	//events
 	public delegate void HitEvent(object sender, object args); public HitEvent OnHit;
 
+	void Start(){
+		sprite = gameObject.GetComponent<SpriteRenderer>();
+	}
+
+	void OnMouseEnter() {
+		sprite.color = Color.cyan;
+	} 
+
+	void OnMouseExit() {
+		sprite.color = Color.white;
+	}
 }
