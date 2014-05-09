ceac2205bb3f2fc5cd4c0d87b36cf0cbf698c33c
Ds886 <daniel.ashkenazi@gmail.com>
2014-05-09 20:13:13 +0300

    Enemy base function and following object, spawning mechanic
    Base types added,
    Script added for enemy to follow
    Basic script added for enemy to spawn another(need to add more control
    to it).

---
+++ Assets/Scripts/Enemy/EnemyFollower.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyFollower : Enemy {
+	
+	// Use this for initialization
+	void Start () {
+		//OnMove = delegate( MoveTowardTarget);
+		Target = GameObject.FindGameObjectWithTag ("Player");
+		StartMoving();
+	}
+	
+	
+	public void Move()
+	{
+		if(Target != null)
+		{
+			float fStep = Speed * Time.deltaTime;
+			this.transform.position = Vector3.MoveTowards(transform.position, 
+			                                              Target.transform.position, 
+			                                              fStep);
+		}
+	}
+	
+	
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (isMoving == true)
+		{
+			Move();
+		}
+
+	}
+}
