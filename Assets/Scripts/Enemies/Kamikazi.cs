e2c6b96f969931c965edbb9a099c0fa9448d6ea2
sagivo <sagiv4@gmail.com>
2014-05-13 19:03:49 -0400

    find target

--- Assets/Scripts/Enemies/Kamikazi.cs
+++ Assets/Scripts/Enemies/Kamikazi.cs
@@ -4,10 +4,15 @@ using System.Collections;
 public class Kamikazi : Enemy {
 
 	void Start () {
-
+		findTarget();
 	}
 
 	void Update () {
 	
+	}
+
+	void findTarget() {
+		Target = Helpers.CloestToObject(Game.Canons.ToArray(), gameObject);
+		if (Target == null) Invoke("findTarget", 2);		
 	}
 }
--- Assets/Scripts/Helpers.cs
+++ Assets/Scripts/Helpers.cs
@@ -1,8 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class Helpers {
-	public GameObject CloestToObject(GameObject[] list, GameObject obj){
+public static class Helpers {
+
+	public static GameObject CloestToObject(object[] list, GameObject obj){
+		List<GameObject> l = new List<GameObject>();
+		foreach(var o in list){
+			if (o is GameObject) l.Add(o as GameObject);
+		}
+		return CloestToObject(l, obj);
+	}
+
+	public static GameObject CloestToObject(List<GameObject> list, GameObject obj){
+		if (list.Count == 0) return null;
 		float minDistance = 100000f;
 		GameObject closest = new GameObject();
 		foreach(var entity in list){
