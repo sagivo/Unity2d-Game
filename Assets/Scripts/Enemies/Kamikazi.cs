ee085b61a672e5768a8a66f22eec18035b9c3742
sagivo <sagiv4@gmail.com>
2014-05-14 18:51:09 -0400

    lookat

--- Assets/Scripts/Enemies/Kamikazi.cs
+++ Assets/Scripts/Enemies/Kamikazi.cs
@@ -6,11 +6,17 @@ public class Kamikazi : Enemy {
 	void Start () {
 		findTarget();
 	}
-
+	 
 	void Update () {
 		if (Target != null){
 			//transform.LookAt(Target.transform);
 			transform.position = Vector3.MoveTowards(transform.position, Target.transform.position, Speed*Time.deltaTime);
+
+			transform.LookAt(Target.transform.position, Vector3.forward);
+
+			//Vector3 dir = Target.transform.position - transform.position;
+			//float angle = Mathf.Atan2(dir.y,dir.x) * Mathf.Rad2Deg;
+			//transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 		}
 	}
 
