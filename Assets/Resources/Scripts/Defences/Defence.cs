1ad3f066223d31577b602364974a52aeca2054ba
sagivo <sagiv4@gmail.com>
2014-05-22 16:29:39 -0400

    type defence

---
+++ Assets/Resources/Scripts/Defences/Defence.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Defence : Liveable {
+
+	protected  new void Start () {
+		base.Start();
+
+		Game.Defences.Add (this);
+		OnDie += () => { Game.Defences.Remove (this); };
+	}
+
+}
