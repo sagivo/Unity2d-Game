c7c8e3ad57dc5c0b5094195735a1df6526810fda
sagivo <sagiv4@gmail.com>
2014-10-17 01:22:35 -0400

    mineral miners

---
+++ Assets/Resources/Scripts/MineralMiner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MineralMiner : Liveable {
+
+	protected new void Start () {
+		base.Start();
+		Game.mineralMiners.Add (this);			
+	}
+	
+	protected new void Update () {
+		base.Update();
+	}
+}
