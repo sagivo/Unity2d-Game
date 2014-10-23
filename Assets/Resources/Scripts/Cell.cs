e6994816204791b37ff499e5208b7750079c9678
sagivo <sagiv4@gmail.com>
2014-10-22 23:24:08 -0400

    animations

--- Assets/Resources/Scripts/Cell.cs
+++ Assets/Resources/Scripts/Cell.cs
@@ -6,12 +6,9 @@ public class Cell : BaseObj {
 	public enum cellType {Empty, Player, Canon, Building};
 	public cellType type;
 	public bool selected;
-	public static float longClickTime = .5f;
-	static float lastClick;
 
 	//colors
 	public Color ColorSelected = Color.red;
-	public Color ColorOver = Color.cyan;
 	public Color ColorBase = Color.white;
 
 	SpriteRenderer sprite;
@@ -22,39 +19,24 @@ public class Cell : BaseObj {
 		base.Start();
 		sprite = GetComponent<SpriteRenderer>();
 		Game.cells.Add(this);
-		sprite = gameObject.GetComponent<SpriteRenderer>();
 	}
 
 	protected new void Update(){
 		base.Update();
-		if (Input.GetMouseButtonDown(0)) OnMouseDown();
-		if (Input.GetMouseButtonUp(0)) OnMouseUp();
-	}
-	
-	void OnMouseDown() {
-		if (lastClick == 0) lastClick = Time.time;
-	}
-
-	void OnMouseUp() {
-		if (lastClick != 0 && lastClick + longClickTime <= Time.time) CastRay();
-		lastClick = 0;
 	}
 	
 	void OnDestroy(){
 		Game.cells.Remove(this);
 	}
 
-	
-	void CastRay() {
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-		var hit = Physics2D.Raycast(ray.origin,Vector2.zero ,100 , ( 1 << LayerMask.NameToLayer("Cells") ));
-		if (hit.collider){
-			//Debug.Log(hit.collider.tag + " " + hit.collider.gameObject.name);
-			hit.collider.GetComponent<SpriteRenderer>().color = ((hit.collider.GetComponent<SpriteRenderer>().color == ColorSelected) ? ColorBase : ColorSelected);
-			//selected = !selected;
+	public void toggleSelect(){
+		/*
+		foreach(Cell c in Game.cells){
+			if (c == this) sprite.color = ((sprite.color == ColorSelected) ? ColorBase : ColorSelected);
+			else c.sprite.color = ColorBase;
 		}
-		//*/
+		*/
+		sprite.color = ((sprite.color == ColorSelected) ? ColorBase : ColorSelected);
 	}
 
 }
---
+++ Assets/Resources/Scripts/UI/Menue.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menue : BaseObj {
+	public static float longClickTime = .8f;
+	Animator anim;
+	float openMenueTime = 0;
+
+	protected new void Start () {
+		base.Start();
+		//anim = GetComponent<Animator>();
+		//anim.SetTrigger("Open");
+	}
+	
+	protected new void Update () {
+		base.Update();
+		if (Input.GetMouseButtonDown(0) && (openMenueTime == 0)) openMenueTime = Time.time + longClickTime;
+		else if (Input.GetMouseButtonUp(0)) openMenueTime = 0;
+		if (openMenueTime != 0 && openMenueTime <= Time.time) openMenue();
+	}
+
+	void openMenue(){
+		openMenueTime = 0;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		var hit = Physics2D.Raycast(ray.origin,Vector2.zero ,100 , ( 1 << LayerMask.NameToLayer("Cells") ));
+		if (hit.collider){
+			var c = hit.collider.GetComponent<Cell>();
+			if (c) c.toggleSelect();
+		}
+	}
+}
