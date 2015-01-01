e950e9cfb2b2357e8d621425aa59ca869b05b8ad
sagivo <sagiv4@gmail.com>
2014-12-31 19:45:26 -0500

    pinch to zoom

---
+++ Assets/Resources/Scripts/Camera/SwipeToMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class SwipeToMove : MonoBehaviour
+{
+	public float speed = 2f;
+	public float minX;
+	public float maxX;
+	public float minY;
+	public float maxY;
+
+	void Update() {
+		if (Input.touchCount == 1  && Input.GetTouch(0).phase == TouchPhase.Moved) {
+			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+			transform.Translate(-touchDeltaPosition.x * speed * Time.deltaTime, -touchDeltaPosition.y * speed * Time.deltaTime, 0);
+			//transform.position = new Vector3(trans
+		}
+	}
+
+}
