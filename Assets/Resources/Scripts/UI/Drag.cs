f573703fdfdc832b2ef37e93dafd3420d4099631
sagivo <sagiv4@gmail.com>
2015-01-03 16:56:23 -0500

    a

--- Assets/Resources/Scripts/UI/Drag.cs
+++ Assets/Resources/Scripts/UI/Drag.cs
@@ -11,16 +11,29 @@ public class Drag : BaseObj, IBeginDragHandler, IDragHandler, IEndDragHandler
 	
 	public void OnBeginDrag(PointerEventData eventData)
 	{
-		var canvas = gameObject.FindInParents<Canvas>();
-		l(canvas);
 	}
 
 	public void OnDrag(PointerEventData data)
 	{
 		gameObject.transform.position = data.position;
+
+		Ray ray = Camera.main.ScreenPointToRay(data.position);
+		Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+		var hit = Physics2D.Raycast(ray.origin, ray.direction ,Mathf.Infinity , ( 1 << LayerMask.NameToLayer("Cells") ));
+		//var hit = Physics2D.Raycast(Camera.main.transform.position, gameObject.transform.position +  new Vector3(v2.x,v2.y,1) ,101 , ( 1 << LayerMask.NameToLayer("Cells") ));
+		if (hit.collider != null){
+			hit.collider.GetComponent<Cell>().select();
+		}
+
 	}
 	
 	public void OnEndDrag(PointerEventData eventData)
 	{
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		l ("boom");
+		if (coll.gameObject.tag == "Cell") l ("Cell");
+		
 	}
 }
